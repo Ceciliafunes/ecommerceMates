8a1ac7242dbc2716cd439b0ebc5a757ef8ef0876
Ceciliafunes <funes.ceciliagisela@gmail.com>
2025-03-12 21:32:43 -0300

    deploy final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // Configuración de la construcción
+  build: {
+    // Opciones de Rollup para personalizar cómo se manejan los chunks
+    rollupOptions: {
+      output: {
+        // Configuración para dividir el código en chunks más pequeños
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Estos son los módulos que deseas separar en su propio chunk
+        },
+      },
+    },
+    // Opción para aumentar el límite de tamaño de los chunks si prefieres evitar la advertencia
+    chunkSizeWarningLimit: 1000, // En KB, ajusta este valor si prefieres un límite mayor (por ejemplo, 1000 KB)
+  },
+
+  // Si estás usando rutas base en tu aplicación (por ejemplo, para Vercel o entornos específicos)
+  base: '/', // Ajusta esto si estás utilizando una ruta base personalizada, por ejemplo, '/mi-app/'
+});
