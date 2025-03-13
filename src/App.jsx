ac2500c452cc58ffce71fc772f93c8b0af1ec752
Ceciliafunes <funes.ceciliagisela@gmail.com>
2025-03-12 22:17:26 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <CartProvider>
         <NavBar />
         <Routes>
